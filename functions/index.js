ac333eacd5955df561af47f9b91f6a3a3de9f509
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-04-15 18:06:36 -0500

    function basic setup

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const serviceAccount = require("./credentials.json");
+
+const app = express();
+app.use(cors());
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.get("/", (req, res) => {
+  return res.status(200).json({message: "Hello world"});
+});
+
+exports.app = functions.https.onRequest(app);
