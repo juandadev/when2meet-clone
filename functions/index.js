bcd80147d2f73e80c2028439248ff02d1b81d63b
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-04-17 12:14:35 -0500

    services endpoints created

--- functions/index.js
+++ functions/index.js
@@ -6,14 +6,52 @@ const cors = require("cors");
 const serviceAccount = require("./credentials.json");
 
 const app = express();
-app.use(cors());
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-app.get("/", (req, res) => {
-  return res.status(200).json({message: "Hello world"});
+const db = admin.firestore();
+app.use(cors());
+
+
+// ==== EVENTS =====
+// create an event and return it's id.
+app.post("/rest/events", async (req, res) => {
+  let docRef = db.collection("EVENT").doc();
+
+  let data = req.body;
+
+  data.id = docRef.id;
+  data.active = true;
+  data.createdAt = new Date();
+
+  await docRef.set(data)
+  .then(response => {
+    return res.status(200).json({message: "Success", eventId: docRef.id});
+  })
+  .catch(error => {
+    return res.status(500).json({message: error});
+  })
+
 });
+
+// get event information
+app.get("/rest/events/:id", async (req, res) => {
+  let docRef = db.collection("EVENT").doc(req.params.id)
+  let doc = await docRef.get()
+
+  if (!doc.exists) {
+    console.log('No such document!');
+      return res.status(500).json({error: "No such document", data: null });
+  } else {
+    console.log('Document data:', doc.data());
+    return res.status(200).json({message: "success", data: doc.data() })
+  }
+});
+
+app.post("/rest/events/:id", async (req, res) => {
+
+});
+
 
 exports.app = functions.https.onRequest(app);
